dcfc53c7362f7c023d5e2f619daa61528aff1925
HappyinessHome <1434613085@qq.com>
2024-11-14 17:26:43 +0800

    增加部分测试

--- src/main/java/com/xiaoma/obsidiantonotion/service/GetImageFromMarkUpload.java
+++ src/main/java/com/xiaoma/obsidiantonotion/service/GetImageFromMarkUpload.java
@@ -1,11 +1,14 @@
 package com.xiaoma.obsidiantonotion.service;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Slf4j
 public class GetImageFromMarkUpload {
     public void getImage(File file){
         // 读取文件内容
@@ -13,13 +16,18 @@ public class GetImageFromMarkUpload {
         try {
             content = new String(Files.readAllBytes(file.toPath()));
             // 使用正则表达式匹配所有图片路径
-            Pattern pattern = Pattern.compile("!\\[.*?\\]\\((.*?)\\)");
+            Pattern pattern = Pattern.compile("!\\[\\[(.*?)\\]\\]\n");
             Matcher matcher = pattern.matcher(content);
+            String updatedContent = content;
+            while (matcher.find()) {
+                String oldImagePath = matcher.group(1);  // 获取图片的原路径
+                log.info("imagePath:{}",oldImagePath);
 
-            // 使用正则表达式匹配并替换图片路径
-            String updatedContent = content.replaceAll(Pattern.quote(oldPath), Matcher.quoteReplacement(newUrl));
+            }
+
+
             // 写回文件
-            Files.write(file.toPath(), updatedContent.getBytes());
+           // Files.write(file.toPath(), updatedContent.getBytes());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/test/java/com/xiaoma/obsidiantonotion/ObsidianToNotionApplicationTests.java
+++ src/test/java/com/xiaoma/obsidiantonotion/ObsidianToNotionApplicationTests.java
@@ -1,13 +1,21 @@
 package com.xiaoma.obsidiantonotion;
 
+import com.xiaoma.obsidiantonotion.service.GetImageFromMarkUpload;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.File;
+
+
 class ObsidianToNotionApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
+    @Test
+    void testGetImagePath(){
+        GetImageFromMarkUpload getImageFromMarkUpload=new GetImageFromMarkUpload();
+        File file=new File("E:\\tmp\\note\\计算机知识\\计算机网络\\王道real\\1.信息时代的计算机网络.md");
+        getImageFromMarkUpload.getImage(file);
+    }
 }
